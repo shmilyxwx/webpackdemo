ad36b88978b93d627d7f1a2f20d64ad28fe8972e
xwx <291857648@qq.com>
2018-04-12 14:13:34 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import sum from './sum'
+import './css/main.scss'
+var  s  = sum(1,5)
+
+console.log(s)
+
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['env']
+				}
+			}
+		},{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+	}
+}
